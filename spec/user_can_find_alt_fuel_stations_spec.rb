6dbb258edb06c0feb10076afaf1141bfea609843
Allan <amaxwellblair@gmail.com>
2016-04-18 09:21:20 -0600

    Created first feature test

---
+++ spec/user_can_find_alt_fuel_stations_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.feature "user can find alt fuel" do
+  scenario "sees alt fuel in 6 mils" do
+    VCR.use_cassette 'nrel/fuel_station' do
+      visit "/"
+      fill_in "Zip Code", with: 80203
+      click_on "Locate"
+
+      expect(current_path).to eq("/search?zip=80203")
+
+      expect(page).to have_content "Name: First one"
+      expect(page).to have_content "Address: Somewhere lane"
+      expect(page).to have_content "Fuel type: Grease"
+      expect(page).to have_content "Distance: 1 mile"
+      expect(page).to have_content "Access Times: 10pm"
+    end
+  end
+end
